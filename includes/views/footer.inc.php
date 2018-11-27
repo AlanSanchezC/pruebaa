8ec90f11ef7e21e873df5da30cca8f93c733f98c
AlanSanchezC <acazarez58@gmail.com>
2018-11-27 03:51:36 +0000

    p

---
+++ includes/views/footer.inc.php
@@ -0,0 +1,17 @@
+<hr />
+<footer>
+    <center>
+        &copy; Direcci&oacute;n de Sistemas - Derechos Reservados 
+	</center>
+</footer>
+
+<script>
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('service-worker.js')
+           .then(function() { console.log("Service Worker Registered"); });
+}
+</script>
+</body>
+
+</html>
